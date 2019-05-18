cc8497edf5079ed9df10d4a44ab10880d67a9d9a
John Oro <johnmichaeloro@gmail.com>
2019-05-18 08:31:10 -0600

    starting over from scratch

--- editor-back-end/models/entry.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const entrySchema = new mongoose.Schema({
-  title: String,
-  description: String
-});
-
-mongoose.exports = mongoose.model('Entry', entrySchema);
--- editor-front-end/src/EntryContainer/EntryContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-class EntryContainer extends Component {
-  render(){
-    return(
-      console.log("hi");
-    )
-  }
-}
