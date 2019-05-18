abdb520358a37bcac67b089bd5bd7ac9a3794279
John Oro <johnmichaeloro@gmail.com>
2019-05-18 11:31:31 -0600

    added cors and express session - back end complete

--- back-end-patterns/server.js
+++ back-end-patterns/server.js
@@ -2,12 +2,28 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 app.use(cors());
 
 require('./db/db');
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 const patternController = require('./controllers/patternController');
 
