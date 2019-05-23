5520f183114cfe0ef2fd94204c91bbea26e394d0
John Oro <johnmichaeloro@gmail.com>
2019-05-23 15:54:14 -0600

    added indexcontainer

---
+++ front-end-patterns/src/PatternContainer/IndexContainer/IndexContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+//I'm probably going to need the same functionality from the PatternList.
+//I will have entriese for the different types of patterns.
+//How do I pull this list of types of patterns?
+//I will pull the pattern types just like I pulled thee entries in PatternList.
+//For each patteren type, I will have an entry appeaer on the page consisting of a pattern title, a colored example, as well as a description that trails off.
+//The user will be able to click anywhere on the entry to be taken to a modified version of the patternlist. This list will contain all of the patterns of the specific type.
+//I will need to modify the get all patterns function in pattern container so it calls this index.
